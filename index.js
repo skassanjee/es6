4c653fc0284b829edd857604469d90eaf363b04e
Sahil <skassanjee@gmail.com>
2020-07-07 04:54:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//strings
+//template literals
+
+let firstName = 'john'
+let lastName = 'smith'
+console.log( `my name is ${firstName} ${lastName}`)
+
+//returns a boolean
+//true
+lastName.startsWith('s')
+//false
+lastName.endsWith('x')
+//true
+lastName.includes('i')
+
+
+//arrow functions
+
